ae3da3601f11bc98ea55311009399eee3d1c95ce
Namrata Jaiswal <76630286+NAMRATA1214@users.noreply.github.com>
2022-07-05 13:11:03 +0530

    Add files via upload
    Typecasting Program

---
+++ TypeCasting.java
@@ -0,0 +1,11 @@
+class TypeCasting
+	{
+		public static void main(String[] args)
+		{
+			char letter='A';
+			int ascii=letter;
+			char alp=(char)ascii;
+			System.out.println(" int value : " +ascii);
+			System.out.println(" char value : " +alp);
+		}
+	}
