7431a344a6c683b9bd9949741d460016570ecf50
Namrata Jaiswal <76630286+NAMRATA1214@users.noreply.github.com>
2022-07-05 12:19:49 +0530

    Add files via upload

--- SwapNumber.java
+++ SwapNumber.java
@@ -1,12 +1,12 @@
 class SwapNumber
-{
-public static void main(String[] args)
-{
-int a=10,b=20,c;
-a=a+b; // a=10+20
-b=a-b; //b=30-20
-a=a-b; //a=30-10
-System.out.println(+ a + " " + b );
-}
-}
+    {
+      public static void main(String[] args)
+          {
+		int a=10,b=20,c;
+		a=a+b; // a=10+20
+		b=a-b; //b=30-20
+		a=a-b; //a=30-10
+		System.out.println(a + " " + b );
+	     }
+	}
 
