0f361b9b8280c8bf1573a13dad120333f2fcd53c
Namrata Jaiswal <76630286+NAMRATA1214@users.noreply.github.com>
2022-07-06 13:56:52 +0530

    Add files via upload
    ScannerExample program

--- ScannerExample.java
+++ ScannerExample.java
@@ -16,19 +16,24 @@ class ScannerExample
 
 			System.out.println("Enter Student Id:");
 			id=sc.nextInt();
+			System.out.println("Student Id:" +id);
 
 			System.out.println("Enter Gender:" );
 			gender=sc.next().charAt(0);
+			System.out.println("Gender:" +gender);
 
 			System.out.println("Enter Age:");
 			age=sc.nextByte();
+			System.out.println("Age:" +age);
 
 
 			System.out.println("Enter Mobile no :" );
 			mobileno=sc.nextLong();
+			System.out.println("Mobile no :" +mobileno);
 
 			System.out.println("Enter CGPA:" );
 			cgpa=sc.nextFloat();
+			System.out.println(" CGPA:" +cgpa);
 
 
 
