f90cfd6dfa8745c4a9e61afdd2f50b5d13e13a40
Namrata Jaiswal <76630286+NAMRATA1214@users.noreply.github.com>
2022-07-13 13:27:37 +0530

    Add files via upload
    Ascending Descending Program

---
+++ AscendingDescending.java
@@ -0,0 +1,34 @@
+//Program to print ascending to descending numbers from 1-10
+class AscendingDescending
+	{
+		public static void main(String[] args)	
+			{
+				AscendingDescending obj =new AscendingDescending();
+				obj.Ascending();
+				obj.Descending();
+			}
+	
+
+public void Ascending()
+	{
+
+		System.out.println("Numbers from Ascending to Descending order for 1 to 10 is :");
+		for(int i=1;i<=10;i++)
+			{					
+				System.out.println(" "+i);
+			}
+		
+	}
+
+public void Descending()
+	{
+
+		System.out.println("Numbers from Descending to Ascending order for 1 to 10 is :");
+		for(int j=10;j>=1;j--)
+			{					
+				System.out.println(" "+j);
+			
+			}	
+
+	}
+}
