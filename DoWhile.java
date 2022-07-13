2d5dc9ef94c2660fc1138d7929183fe439e3c1b6
Namrata Jaiswal <76630286+NAMRATA1214@users.noreply.github.com>
2022-07-13 13:44:15 +0530

    Add files via upload
    Do While Program

---
+++ DoWhile.java
@@ -0,0 +1,15 @@
+/* do-while loop  i = 11 and i>3.
+for-loop  j=10 and j>6 */
+
+class DoWhile
+	{
+		public static void main(String[] args)
+			{
+				int i=11;
+				do
+				{
+				 	System.out.println(+i);
+					i--;
+				}while(i>3);
+			}
+	}
